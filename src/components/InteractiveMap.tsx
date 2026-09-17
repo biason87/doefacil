f4604d193b3a18f9ba689332030c5c771fc0c7ec
biason <biasonket@gmail.com>
2026-09-17 15:47:24 -0300

    Update InteractiveMap.tsx

--- src/components/InteractiveMap.tsx
+++ src/components/InteractiveMap.tsx
@@ -137,10 +137,10 @@ export default function InteractiveMap() {
         className="w-full h-full z-10"
         zoomControl={false}
       >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-        />
+       <TileLayer
+  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+/>
         <ChangeView center={center} zoom={zoom} />
         
         {userLocation && (
